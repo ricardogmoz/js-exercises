70e29332f7973d6692704b35345c4c5e19cb1413
Ricardo Gonzalez <ricvrdo.gonzalez@gmail.com>
2025-07-13 19:22:57 -0600

    muiButton: Add MUI Button component for Week 2 challenge

---
+++ week2/muiButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Button from '@mui/material/Button';
+
+
+function MuiButtonExample() {
+
+  return (
+
+    <div style={{ padding: 20 }}>
+
+      <Button variant="contained" color="primary">
+
+        Hello MUI
+
+      </Button>
+
+    </div>
+
+  );
+
+}
+
+export default MuiButtonExample;
+
